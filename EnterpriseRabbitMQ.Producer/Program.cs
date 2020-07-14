8e5cc59b767ca919201e74d55971e69a961bbe37
Pedro <pedrodxavier@hotmail.com>
2020-07-14 16:09:13 -0300

    Add project files.

---
+++ EnterpriseRabbitMQ.Producer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace EnterpriseRabbitMQ.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+
+                int counter = 1;
+                while (true)
+                {
+                    string message = $"{counter ++} - Message... :)";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                    Thread.Sleep(100);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
